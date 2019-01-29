12452e0a0b77829c452a31f689f97b7b9888aae0
Jason Jocsin <jsnjocsin@gmail.com>
2019-01-30 07:51:29 +0800

    init

--- comhavenapp/views.py
+++ comhavenapp/views.py
@@ -69,7 +69,7 @@ def auto_login(request, login_id):
             # chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
             # opts = ChromeOptions()
             # opts.binary_location = chrome_bin
-            browser = webdriver.Chrome(executable_path="chromedriver.exe")
+            browser = webdriver.Chrome(executable_path="chromedriver")
             browser.get(login.login_target_url)
             username = browser.find_element_by_id("edit-mail")
             username.send_keys(login.login_username)
