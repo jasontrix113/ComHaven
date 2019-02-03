96aa751beecb39bee4ebb08b15d6a14e89b12010
Jason Jocsin <jsnjocsin@gmail.com>
2019-02-04 00:50:47 +0800

    asdasfas

--- comhavenapp/views.py
+++ comhavenapp/views.py
@@ -67,13 +67,8 @@ def auto_login(request, login_id):
 
     if login:
         if login.login_name == 'Schoology':
-            chrome_exec_shim = settings.BASE_DIR+'\chromedriver\chromedriver.exe'
-            print(chrome_exec_shim)
-            opts = webdriver.ChromeOptions()
-            opts.binary_location = chrome_exec_shim
-            opts.add_argument("--no-sandbox");
-            opts.add_argument("--disable-gpu");
-            browser = webdriver.Chrome(executable_path = chrome_exec_shim, chrome_options=opts)
+
+            browser = webdriver.Chrome()
             browser.get(login.login_target_url)
             username = browser.find_element_by_id("edit-mail")
             username.send_keys(login.login_username)
