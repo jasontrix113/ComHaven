e4849bf95d4d47f910d73c7e3f0212ee7838789c
Jason Jocsin <jsnjocsin@gmail.com>
2019-02-06 23:33:39 +0800

    a

--- comhavenapp/views.py
+++ comhavenapp/views.py
@@ -71,6 +71,7 @@ def auto_login(request, login_id):
             opts = webdriver.ChromeOptions()
             opts.binary_location = chrome_exec_shim
             opts.add_argument('--disable-gpu')
+            opts.add_argument('--no-sandbox')
             browser = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=opts)
             browser.get(login.login_target_url)
             username = browser.find_element_by_id("edit-mail")
