603ef205ded3eae8a4307bd6f3276e9e336c6495
Jason Jocsin <jsnjocsin@gmail.com>
2019-02-06 23:57:02 +0800

    a

--- comhavenapp/views.py
+++ comhavenapp/views.py
@@ -70,6 +70,7 @@ def auto_login(request, login_id):
             chrome_exec_shim = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
             opts = webdriver.ChromeOptions()
             opts.binary_location = chrome_exec_shim
+            opts.add_argument('--headless')
             opts.add_argument('--disable-gpu')
             opts.add_argument("--no-sandbox")
             browser = webdriver.Chrome(executable_path=chrome_exec_shim, chrome_options=opts)
